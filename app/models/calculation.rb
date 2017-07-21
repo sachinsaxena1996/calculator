6cfdbd741bea5ca388a5fbe7962d6583d4987056
sachinsaxena1996 <sachinsaxena1996@gmail.com>
2017-07-22 00:29:20 +0530

    calculator for evalatuating expressions

--- app/models/calculation.rb
+++ app/models/calculation.rb
@@ -1,22 +1,62 @@
 class Calculation < ApplicationRecord
-  def evaluate_string(calculation_string)
-    # 5*3+1+6/2+9*100
-    new_calc_str = calculation_string.split('+')
-    calc_output = 0
-    new_calc_str.each do |elem|
-      if elem.length > 1 && !elem.index('*').nil?
-        arr = elem.split('*')
-        calc_output = calc_output + (arr[0].to_i * arr[1].to_i)
-      elsif elem.length > 1 && !elem.index('/').nil?
-        arr = elem.split('/')
-        calc_output = calc_output + (arr[0].to_i / arr[1].to_i)
-      elsif elem.length > 1 && !elem.index('-').nil?
-        arr = elem.split('-')
-        calc_output = calc_output + (arr[0].to_i - arr[1].to_i)
-      else
-        calc_output = calc_output + elem.to_i
+  def evaluate_string(input)
+    values = []
+    operators = []
+    input = input.scan(/\w+|\W/)
+    input.each_with_index do |token, index|
+      if ((token != "*") && (token != "+") && (token != "-") && (token != "/"))
+        input[index] = token.to_i
       end
     end
-    calc_output
+    input.each do |val|
+      if val.class == Fixnum
+        values << val
+      elsif val.class == String
+        if (val == '+' || val == '-') && (operators.last == '*')
+          calc = 0
+          calc = values[-2] * values[-1]
+          values.pop
+          values.pop
+          values << calc
+          operators.pop
+          redo
+        elsif (val == '+' || val == '-') && (operators.last == '/')
+          calc = 0
+          calc = values[-2] / values[-1]
+          values.pop
+          values.pop
+          values << calc
+          operators.pop
+          redo
+        elsif (val == '+' || val == '-') && (operators.last == '+')
+          calc = 0
+          calc = values[-2] + values[-1]
+          values.pop
+          values.pop
+          values << calc
+          operators.pop
+          redo
+        elsif (val == '+' || val == '-') && (operators.last == '-')
+          calc = 0
+          calc = values[-2] - values[-1]
+          values.pop
+          values.pop
+          values << calc
+          operators.pop
+          redo
+        else
+          operators.push(val)
+        end
+      end
+    end
+
+    output = 0
+    if values.length > 2
+      output = values[-2].send(operators[-1], values[-1]) if !operators[-1].nil?
+      output = output.send(operators[0], values[0]) if !operators[0].nil?
+    else
+      output = values[0].send(operators[0], values[1])
+    end
+    output
   end
 end
--- spec/models/calculation_spec.rb
+++ spec/models/calculation_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Calculation, type: :model do
   it "evaluates the expression correctly" do
     c1 = Calculation.new
     expect(c1.evaluate_string('5*3+1+6/2+9*100')). to eq(919)
+    expect(c1.evaluate_string('5*3+1+6/85+9*100')). to eq(916)
+    expect(c1.evaluate_string('6/2-5*3+1+9*100')). to eq(889)
   end
 end
